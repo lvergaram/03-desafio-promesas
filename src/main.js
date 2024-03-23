d8ce661b7cbf815fd7c1b37b9aec08d17249439e
lvergaram <lvergaramena@gmail.com>
2024-03-23 01:39:48 -0300

    async functions

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 const API_URL = 'https://jsonplaceholder.typicode.com/photos'
 
-const getAlbumListrByUrl = async (url,cantidad=1) => {
-    try{
+const getAlbumListrByUrl = async (url, cantidad = 1) => {
+    try {
         const response = await fetch(url)
         const data = await response.json()
-        const albumList = data.slice(0,cantidad)
-        console.log(albumList)
-    } catch(err){
+        const albumList = data.slice(0, cantidad)
+        albumList.forEach(album => { console.log(album) })
+    } catch (err) {
         console.log(err)
     }
 }
 
-getAlbumListrByUrl(API_URL,20)
+const trySendInfo = () => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res('información enviada....')
+        }, 3000)
+    })
+}
+
+const sendInfo = async() => {
+    const mgsSended = await trySendInfo()
+    console.log(mgsSended)
+}
+
+getAlbumListrByUrl(API_URL, 20)
+sendInfo()
